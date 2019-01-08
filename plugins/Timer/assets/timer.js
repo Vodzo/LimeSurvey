99164664e026d9bed3890ccfaa91c52e1de2609f
Vojislav Zelic <vojislav.zelic@infostud.com>
2019-01-08 11:33:51 +0100

    fixed timer submit js

--- plugins/Timer/assets/timer.js
+++ plugins/Timer/assets/timer.js
@@ -1,37 +1,42 @@
-$(document).ready(function() {
-    (function() {
-        if( window.Timer ) {
-             function startTimer(duration) {
-                var timer = duration, minutes, seconds;
-                var ticker = setInterval(function () {
-                    minutes = parseInt(timer / 60, 10);
-                    seconds = parseInt(timer % 60, 10);
-            
-                    minutes = minutes < 10 ? "0" + minutes : minutes;
-                    seconds = seconds < 10 ? "0" + seconds : seconds;
-                    
-                    $(window).trigger('timer:tick', {
-                        "display": minutes + ":" + seconds,
-                        "timer": timer
-                    });
-                    
-                    if (--timer < 0) {
-                        clearInterval(ticker);
-                        $(window).trigger('timer:done');
-                    }
-                }, 1000);
-            }
-            
-            startTimer(Timer.end - Timer.current);
-        }
-        
-        $(window).on('timer:tick', function(event, data) {
-			if(data.timer > 0) {
+$(document).ready(function () {
+	(function () {
+		if (window.Timer) {
+			function startTimer(duration) {
+				var timer = duration, minutes, seconds;
+				var ticker = setInterval(function () {
+					minutes = parseInt(timer / 60, 10);
+					seconds = parseInt(timer % 60, 10);
+
+					minutes = minutes < 10 ? "0" + minutes : minutes;
+					seconds = seconds < 10 ? "0" + seconds : seconds;
+
+					$(window).trigger('timer:tick', {
+						"display": minutes + ":" + seconds,
+						"timer": timer
+					});
+
+					if (--timer < 0) {
+						clearInterval(ticker);
+						$(window).trigger('timer:done');
+					}
+				}, 1000);
+			}
+
+			startTimer(Timer.end - Timer.current);
+		}
+
+		$(window).on('timer:tick', function (event, data) {
+			if (data.timer > 0) {
 				$('.timer').html(data.display);
 			}
-        });
-        $(window).on('timer:done', function(event, data) {
-            $('#ls-button-submit').val('movesubmit').click();
-        });
-     })();
+		});
+		$(window).on('timer:done', function (event, data) {
+			$("<input/>", {
+				'type': "hidden",
+				'name': "move",
+				'value': "movesubmit",
+			}).appendTo('form#limesurvey');
+			$('form#limesurvey').submit();
+		});
+	})();
 });
